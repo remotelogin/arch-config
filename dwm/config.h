a1fcf8f35bb944f413a295873e5fab8f39c89be5
snowy <150125982+fagdogboy@users.noreply.github.com>
2024-12-03 22:01:40 +0100

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -62,7 +62,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *screenshot[] = {"/home/snowy/aur/screenshot/screenshot.sh", NULL};
+static const char *screenshot[] = {"/home/snowy/aur/arch-config/screenshot/screenshot.sh", NULL};
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
