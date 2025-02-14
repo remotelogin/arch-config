d80975af8fa6cb870edd5656f738edc8ca147b14
snowy <150125982+kourna@users.noreply.github.com>
2025-02-14 09:26:38 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
-sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot hdparm emacs awk bc base-devel libx11 libxft libxinerama 
+sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot hdparm emacs awk bc base-devel libx11 libxft libxinerama pavucontrol firefox obs-studio feh  
+sudo cp -r screen ../
 sudo timedatectl set-timezone Europe/Berlin
 
 echo "installed all deps, compile dwm / st manually and then copy your preffered xinit"
